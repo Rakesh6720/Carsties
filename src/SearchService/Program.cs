b9cd9d352d595fec7c5acb1ce17bfb78ec1c5505
Rakesh Baruah <rbaruah@gmail.com>
2025-03-28 09:42:20 -0500

    Implement SearchService with MongoDB integration, including search functionality and database initialization

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,8 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Modes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +14,14 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseAuthorization();
+
 app.MapControllers();
+
+try {
+    await DbInitializer.InitDb(app);
+} catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+}
 
 app.Run();
