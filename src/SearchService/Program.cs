ed92c0da5d85eab53956da5af90bcb7358352421
Rakesh Baruah <rbaruah@gmail.com>
2025-03-28 10:09:56 -0500

    Add Polly for resilient HTTP requests and refactor database initialization in SearchService

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,7 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Modes;
 using SearchService.Services;
@@ -11,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -21,10 +23,17 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try {
-    await DbInitializer.InitDb(app);
-} catch (Exception ex) {
-    Console.WriteLine(ex.Message);
-}
+app.Lifetime.ApplicationStarted.Register(async () => {
+    try {
+        await DbInitializer.InitDb(app);
+    } catch (Exception ex) {
+        Console.WriteLine(ex.Message);
+    }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() => 
+    HttpPolicyExtensions.HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
